007124661488e27a630840599b25e89cfd10db1a
marsjc94-ctrl <marsjc94@gmail.com>
2025-10-26 02:47:27 -0400

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-swcMinify: true,
-};
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
